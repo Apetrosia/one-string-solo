6d28813663148124adc8551976611f75de286fc4
Анна Петросян <apetrosia@sfedu.ru>
2024-08-18 14:38:04 +0300

    Guitar can play

--- Assets/Scripts/String.cs
+++ Assets/Scripts/String.cs
@@ -8,7 +8,10 @@ public class String : MonoBehaviour
     private float posOffset;
     private float mousePosY;
     [SerializeField] private float minScaleY;
+    [SerializeField] private float maxScaleY;
     private float displayHeight;
+
+    private AudioSource source;
 
     [SerializeField] private bool showLog;
 
@@ -16,14 +19,23 @@ public class String : MonoBehaviour
     {
         posOffset = 5f;
         displayHeight = Display.main.systemHeight;
+        source = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         mousePosY = ToUnityCoordinates(Input.mousePosition.y) + posOffset;
         transform.localScale = new Vector3(0.1f, Mathf.Max(minScaleY, 0.5f * Mathf.Min(mousePosY, 9f) / 9f), 1f);
+        if (Input.GetKeyDown(KeyCode.Space))
+            PluckString();
         if (showLog)
             Debug.Log(Input.mousePosition.y + " " + mousePosY + " " + Display.main.systemHeight  + " " + transform.position.y);
+    }
+
+    private void PluckString()
+    {
+        source.pitch = 1 + (transform.localScale.y - 0.1f) * 5 / 2;
+        source.Play();
     }
 
     private float ToUnityCoordinates(float displayPos) => displayPos / displayHeight * 10f - 5f;
