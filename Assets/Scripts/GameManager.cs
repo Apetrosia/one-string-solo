25decb009dc1509283a5970edd072dd730a8db91
Анна Петросян <apetrosia@sfedu.ru>
2024-08-18 20:45:13 +0300

    Add pause UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject pausePanel;
+    [SerializeField] string currentScene;
+    private AudioSource stringObj;
+
     void Start()
     {
-        
+        stringObj = GameObject.Find("String").GetComponent<AudioSource>();
+        pausePanel.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!pausePanel.activeSelf)
+            {
+                pausePanel.SetActive(true);
+                Time.timeScale = 0.0f;
+            }
+            else
+            {
+                pausePanel.SetActive(false);
+                Time.timeScale = 1.0f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(currentScene);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("MainMenu");
     }
+
+    public bool IsPaused() => pausePanel.activeSelf;
 }
