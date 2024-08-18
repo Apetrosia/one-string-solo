0c4640c95b7562efebe7b6ff8eacd0bc413a7018
Apetrosia <92916916+Apetrosia@users.noreply.github.com>
2024-08-18 16:01:24 +0300

    The guitar become more informative (#5)
    * Add string scaling
    
    * Add guitar to lvl1
    
    * Add minScaleY
    
    * Add showLog
    
    * Add Conductor
    
    Music can be played
    
    * Add ChartReader
    
    The chart of the song can be read from file
    
    * Add notes spawn
    
    * Update kuznechik chart
    
    * Guitar can play
    
    * Add notes to the scene
    
    ---------
    
    Co-authored-by: comprock15 <khoruzhaia@sfedu.ru>

--- Assets/Scripts/String.cs
+++ Assets/Scripts/String.cs
@@ -8,7 +8,12 @@ public class String : MonoBehaviour
     private float posOffset;
     private float mousePosY;
     [SerializeField] private float minScaleY;
+    [SerializeField] private float maxScaleY;
+    [SerializeField] private GameObject[] notesLabels;
+    [SerializeField] private float[] Ycoords;
     private float displayHeight;
+
+    private AudioSource source;
 
     [SerializeField] private bool showLog;
 
@@ -16,14 +21,28 @@ public class String : MonoBehaviour
     {
         posOffset = 5f;
         displayHeight = Display.main.systemHeight;
+        source = GetComponent<AudioSource>();
+
+        //for (int i = 0; i < notesLabels.Length; i++)
+            //notesLabels[i].transform.position = new Vector3(notesLabels[i].transform.position.x, Ycoords[i] * (displayHeight / 10f) + displayHeight / 2f, 0f);
+
+        showLog = true;
     }
 
     void Update()
     {
         mousePosY = ToUnityCoordinates(Input.mousePosition.y) + posOffset;
         transform.localScale = new Vector3(0.1f, Mathf.Max(minScaleY, 0.5f * Mathf.Min(mousePosY, 9f) / 9f), 1f);
+        if (Input.GetKeyDown(KeyCode.Space))
+            PluckString();
         if (showLog)
             Debug.Log(Input.mousePosition.y + " " + mousePosY + " " + Display.main.systemHeight  + " " + transform.position.y);
+    }
+
+    private void PluckString()
+    {
+        source.pitch = 1 + (transform.localScale.y - 0.1f) * 5 / 2;
+        source.Play();
     }
 
     private float ToUnityCoordinates(float displayPos) => displayPos / displayHeight * 10f - 5f;
