5e92d4587ae72c20b710f40ea8bd28774a4da074
Анна Петросян <apetrosia@sfedu.ru>
2024-08-19 17:53:16 +0300

    Add flying scores

--- Assets/Scripts/Notes/NoteDetection.cs
+++ Assets/Scripts/Notes/NoteDetection.cs
@@ -14,12 +14,16 @@ public class NoteDetection : MonoBehaviour
 
     private Stack<GameObject> notes;
 
+    private GameObject canvas;
+    [SerializeField] TMP_Text[] grades;
+
     private float maxTime = 3f;
 
     void Start()
     {
         soloString = GameObject.FindAnyObjectByType<String>();
         gameManager = GameObject.FindObjectOfType<GameManager>();
+        canvas = GameObject.Find("UI");
         notes = new Stack<GameObject>();
     }
 
@@ -30,17 +34,18 @@ public class NoteDetection : MonoBehaviour
         {
             if (notes.Count > 0)
             {
+                noteDetected = true;
                 multiplier = notes.Peek().GetComponent<CircleCollider2D>().radius;
                 float points = multiplier * (int)(3.3f - Mathf.Abs(notes.Peek().transform.position.x - transform.position.x) +
                     3.3 - Mathf.Abs(notes.Peek().transform.position.y - soloString.GetYPos())) * point * 100;
                 gameManager.AddScore((int)points);
-                GradeFly(points);
+                StartCoroutine(GradeFly(points));
                 GameObject.Destroy(notes.Pop().gameObject);
             }
             else
             {
                 gameManager.AddScore(-500);
-                GradeFly(-500f);
+                StartCoroutine(GradeFly(-500));
             }
         }
     }
@@ -54,15 +59,41 @@ public class NoteDetection : MonoBehaviour
     {
         if (notes.Count > 0)
             notes.Pop();
+        if (!noteDetected)
+        {
+            gameManager.AddScore(-1000);
+            StartCoroutine(GradeFly(-1000f));
+        }
+        noteDetected = false;
     }
 
     private IEnumerator GradeFly(float points)
     {
         System.Random r = new System.Random();
-        float time = maxTime; //1000 1250 1500 -500
-        /*text.text = formulas[f];
+        float time = maxTime;
 
-        TMP_Text obj = Instantiate(text, new Vector3(r.Next(xrange.Item1, xrange.Item2), r.Next(yrange.Item1, yrange.Item2), 0),
+        TMP_Text text = grades[0];
+
+        switch ((int)points)
+        {
+            case -1000:
+                text = grades[0];
+                break;
+            case -500:
+                text = grades[1];
+                break;
+            case 1000:
+                text = grades[2];
+                break;
+            case 1250:
+                text = grades[3];
+                break;
+            case 1500:
+                text = grades[4];
+                break;
+        }
+
+        TMP_Text obj = Instantiate(text, new Vector3(r.Next(-200, 200) + Display.main.systemWidth / 2, 100, 0),
             Quaternion.Euler(new Vector3(0, 0, r.Next(-30, 30))));
         obj.transform.SetParent(canvas.transform);
 
@@ -73,7 +104,7 @@ public class NoteDetection : MonoBehaviour
             yield return new WaitForSeconds(Time.deltaTime);
             time -= Time.deltaTime;
         }
-        GameObject.Destroy(obj.gameObject);*/
+        GameObject.Destroy(obj.gameObject);
         yield return new WaitForSeconds(Time.deltaTime);
     }
 }
